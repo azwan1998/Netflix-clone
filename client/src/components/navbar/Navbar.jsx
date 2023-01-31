6f810f753a0b02d6fc6f9e03981f59a9a452c95a
azwan11 <azwanalkhawarismi@gmail.com>
2023-01-31 12:00:53 +0700

    salah branch

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './navbar.scss';
+import { ArrowDropDown, Search, Notifications } from '@material-ui/icons';
+import { useState } from 'react';
+
+function Navbar() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false:true);
+        return () => (window.onscroll = null);
+    }
+    return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+        <div className="container">
+            <div className="left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="" />
+                <span>homepage</span>
+                <span>Movies</span>
+                <span>Series</span>
+                <span>New and Popular</span>
+                <span>My list</span>
+            </div>
+            <div className="right">
+                <Search className='icon-right'/>
+                <span>KID</span>
+                <Notifications className='icon-right'/>
+                <img src="https://images4.alphacoders.com/127/thumb-1920-1273867.jpg" alt="" />
+                <div className="profile">
+                    <ArrowDropDown className='icon-right'/>
+                    <div className="option">
+                        <span>Settings</span>
+                        <span>Logout</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.scss';
+import Navbar from '../components/navbar/Navbar.jsx';
+
+function Home() {
+  return (
+    <div className="home">
+        <Navbar />
+        <img className='backgrd' src="https://lh3.googleusercontent.com/EZ_J4neUqmIBq62fbCG_RUlW-wqZlBfrgfMFXZUF337pBVr09Pm3EAxdAkHYP5D4S44eMd6I15Kp_6msyKq-4TwWj6t-Ci6JjOeO81-ThOBTP6OCoUylVCwOnkFYqjWucFABJnitiT_I3VHaEV8YcIL67ET3Qsu2ObEgnK1HQ3nhgzIa25IRUEaBb4sImSCaP_vBhg_-CHSyrZEqdCx3-9pX7dknVnfvArY-jSSXkx823sC31W9LNcEti1iQwlk8yYit6fX7BIPTH4V-eZjjlWibmGp-p1hnJ4BzAVKiGXHkxVHAna9Xv7T3QttfBuxYVU2sdSsA1R53okxhNej5tFOm9RHfr9LLUavH8RU-XwMs4LkG4T1Z8_t24BDUtw_1Iq9m7BCC5_YkFRlUFLYHbNBkENDvPckpAcqLnPIXHyhegkHB7hzcFHctkJpuhQwLK1ck4mhBcfDtZ6vYLyD7BXFU8qfBYUDVqFv1P1kJQl5TODW8o2F32cBpM4fxP-5WmMYWuwuxjrHYZ1wwCivsFbfMZMaiaQpmTiAvyt7PhAbt2rTsh3tTPrL07-P4ozVAF3DjLGGGwWd66kAm6m8NIrzqEDRtbDWjkvECmOKWDfpuo3V6fIedcGRWTjP71G97dYoP-mdkm8PzOkLRGVzNScox7-h3RS3ggKpV7_F9CevztKikVAdjXngX5hpiYrdQRgvXlz138GWX5NDYfRRGljhvrji23aXPOi6Uw9mFpesR87QPH73qWUTtOjItuMPaQJ1KH6xCjZvSu20LEA6OE38l3izEIkfz69e4a7DweEWg4RDFsf1X5fBP5y_PkMdP-ByYEdFWiNB-MtbAibK55_CQlAVlC9H5ORCg_3JzgQx3coP4osuQ3Y-RTmLr50uUT0kPneErO1piscVu-YoMhitA_lxffDgL0Yr5GQdlsH-fSoLqW8S4xaAE3AnwcAWS_Igh0Xh5jLAO8EkMtS53-lAWpLoqndH3aBv1GdUsVuM_R5BlXfysLGH7ODtUTDvzYHimZRIN_-Jt2NHlP__PUUHhPYtWNf-C4HPQ_EfNv0Kts5dx75eA3bnj9TYPloqElwuOqDJUJDiBw_BHIjbABTwJsNl8QLZ1mJ-XfJ4GwgLOzIFk7_cyRc_DfTa-MFZ2dgA=w935-h661-no?authuser=0" alt="" />    
+    </div>
+    
+    
+  );
+}
+
+export default Home
