f56fe2173e52c495b907686e1a0c6ddb4b349b76
azwan11 <azwanalkhawarismi@gmail.com>
2023-02-21 09:20:33 +0700

    react-dom install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,25 @@ import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 const App = () => {
   return (
-    <Login />
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/search'
+            element={
+              <div className="app__page">
+              <Login />
+            </div>}/>
+          <Route path='/' 
+            element={
+            <div className="app__page">
+              <Home />
+            </div>}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
